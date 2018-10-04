cffead8ed84b6213633a9b0462bb507a8e375a63
Nicolas Thomas <thomnico@googlemail.com>
2018-10-04 10:11:10 +0200

    Adding cloudify fortimanager plugi as a submodule
    Signed-off-by: Nicolas Thomas <thomnico@googlemail.com>

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cfy blueprint upload -b fortimanager ./blueprint.yaml
-cfy deployment create --skip-plugins-validation fortimanager -b fortimanager
-cfy -vv executions start -d fortimanager install
--- stop-device-group.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cfy -vv executions start -d dg-fortimanager uninstall
-cfy deployments delete dg-fortimanager
-cfy blueprints delete dg-fortimanager
